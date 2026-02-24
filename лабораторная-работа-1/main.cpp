0990911864955fe81d26002974b32d70bc701e84
ssdagv <darasmetanina5@gmail.com>
2026-02-24 09:24:47 +0300

    Update main.cpp

--- лабораторная-работа-1/main.cpp
+++ лабораторная-работа-1/main.cpp
@@ -5,54 +5,70 @@
 
 int main() {
 
-    SmartLight light;  
-    SmartCamera camera("Моя камера"); 
+    SmartLight light("светюля");  
     
-    // проверка  
+    SmartCamera cam1("камера", 8, true, 1920);     
+    SmartCamera cam2("Уличная камера", false);      
+    SmartCamera cam3;                                 
+    
+    // проверка
     std::cout << "\nСВЕТ:" << std::endl;
     std::cout << "  Имя: " << light.getName() << std::endl;
     std::cout << "  Мощность: " << light.getPowerConsumption() << " Вт" << std::endl;
     std::cout << "  Яркость: " << light.getBrightness() << "%" << std::endl;
     std::cout << "  Цвет: " << light.getColorTemp() << std::endl;
     
-    std::cout << "\nКАМЕРА:" << std::endl;
-    std::cout << "  Имя: " << camera.getName() << std::endl;
-    std::cout << "  Мощность: " << camera.getPowerConsumption() << " Вт" << std::endl;
-    std::cout << "  Запись: " << (camera.getIsRecording() ? "да" : "нет") << std::endl;
-    std::cout << "  Разрешение: " << camera.getResolution() << "p" << std::endl;
-
-     std::cout << std::endl;
+    std::cout << "\nКАМЕРА 1:" << std::endl;
+    std::cout << "  Имя: " << cam1.getName() << std::endl;
+    std::cout << "  Мощность: " << cam1.getPowerConsumption() << " Вт" << std::endl;
+    std::cout << "  Запись: " << (cam1.getIsRecording() ? "да" : "нет") << std::endl;
+    std::cout << "  Разрешение: " << cam1.getResolution() << "p" << std::endl;
     
+    std::cout << "\nКАМЕРА 2:" << std::endl;
+    std::cout << "  Имя: " << cam2.getName() << std::endl;
+    std::cout << "  Мощность: " << cam2.getPowerConsumption() << " Вт " << std::endl;
+    std::cout << "  Запись: " << (cam2.getIsRecording() ? "да" : "нет") << std::endl;
+    std::cout << "  Разрешение: " << cam2.getResolution() << "p " << std::endl;
+    
+    std::cout << "\nКАМЕРА 3 :" << std::endl;
+    std::cout << "  Имя: " << cam3.getName() << std::endl;
+    std::cout << "  Мощность: " << cam3.getPowerConsumption() << " Вт" << std::endl;
+    std::cout << "  Запись: " << (cam3.getIsRecording() ? "да" : "нет") << std::endl;
+    std::cout << "  Разрешение: " << cam3.getResolution() << "p" << std::endl;
 
-    SmartDevice* devices[] = {&light, &camera};
+    std::cout << std::endl;
+    
+   SmartDevice* devices[] = {&light, &cam1, &cam2, &cam3};
+    int deviceCount = 4;
     
     // Включение устройств
     std::cout << "Включение:" << std::endl;
-    devices[0]->turnOn();  // Свет
-    devices[1]->turnOn();  // Камера
+    devices[0]->turnOn();  
+    devices[1]->turnOn(); 
+    devices[2]->turnOn(); 
+    devices[3]->turnOn();
     
     // Информация
     std::cout << "\nИнформация:" << std::endl;
     devices[0]->displayInfo();
     devices[1]->displayInfo();
+    devices[2]->displayInfo();
+    devices[3]->displayInfo();
     
     // Уникальные методы
-    std::cout << "\nУникальные методы:" << std::endl;
-    
-    // Для света
-    SmartLight* lightPtr = dynamic_cast<SmartLight*>(devices[0]);
-    lightPtr->dimSmoothly(50);
-    lightPtr->changeColor("Холодный");
-    
-    // Для камеры
-    SmartCamera* cameraPtr = dynamic_cast<SmartCamera*>(devices[1]);
-    cameraPtr->startRecording();
-    cameraPtr->stopRecording();
+    dynamic_cast<SmartLight*>(devices[0])->dimSmoothly(50);
+    dynamic_cast<SmartLight*>(devices[0])->changeColor("Холодный");
+
+    dynamic_cast<SmartCamera*>(devices[1])->startRecording();
+    dynamic_cast<SmartCamera*>(devices[1])->stopRecording();
+
     
     // Выключение
     std::cout << "\nВыключение:" << std::endl;
     devices[0]->turnOff();
     devices[1]->turnOff();
+    devices[2]->turnOff();
+    devices[3]->turnOff();
     
     return 0;
 }
